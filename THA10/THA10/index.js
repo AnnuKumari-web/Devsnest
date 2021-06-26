82f4494db10ea7fd72ef15b9c2f84ef801a2d8a1
Annu Kumari <56755183+AnnuKumari-web@users.noreply.github.com>
2021-06-27 00:45:52 +0530

    updated

---
+++ THA10/THA10/index.js
@@ -0,0 +1,79 @@
+const boxes = document.querySelectorAll('.box');
+const points = document.getElementById('points');
+const moves = document.getElementById('moves')
+
+let hasFlipped = false;
+let lockBoard = false;
+let first, second;
+var point = 0;
+var move = 0 ;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === first) return;
+  move += 1;
+  moves.innerText = `${move}`; 
+
+  this.classList.add('flip');
+
+  if (!hasFlipped) {
+    // first card 
+    hasFlipped = true;
+    first = this;
+    return;
+  }
+
+  // second card 
+  hasFlipped = false;
+  second = this;
+
+  checkForMatch();
+
+}
+// matching
+function checkForMatch() {
+  // is match 
+  if (first.dataset.framework === second.dataset.framework) {
+    point +=1;
+    points.innerText = `${point}`;
+    disableCards();
+  }
+  // not a match 
+  else {
+    unflipCards();
+  }
+}
+
+function disableCards() {
+  first.removeEventListener('click', flipCard);
+  second.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    first.classList.remove('flip');
+    second.classList.remove('flip');
+
+    resetBoard();
+    lockBoard = false;
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlipped, lockBoard] = [false, false];
+  [first, second] = [null, null];
+}
+
+(function shuffle() {
+  boxes.forEach(box => {
+let randonNo= Math.floor(Math.random() * 16);
+box.style.order = randonNo;
+  });
+})();
+
+
+boxes.forEach(box => box.addEventListener('click', flipCard));
