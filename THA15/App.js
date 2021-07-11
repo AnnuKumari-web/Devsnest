cbf16d5ac128eaeab13b17d3b5443fadb67f188f
Annu Kumari <56755183+AnnuKumari-web@users.noreply.github.com>
2021-07-11 13:47:49 +0530

    Add files via upload

---
+++ THA15/App.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h1>Hello, getting started with react</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
